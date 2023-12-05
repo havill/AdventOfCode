bfc75e52803620854b4f3cb71dded753b8d2e2d1
Eido Inoue <eido.a.inoue@rakuten.com>
2023-12-05 21:43:16 +0900

    first gold star day 5

--- 2023/day-05/if-you-give-a-seed-a-fertilizer.go
+++ 2023/day-05/if-you-give-a-seed-a-fertilizer.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -39,9 +40,39 @@ func splitByColon(s string) (string, string, error) {
 	return parts[0], parts[1], nil
 }
 
+func mapSrcToDest(src int, ranges MappingList) int {
+	dest := src
+
+	//fmt.Println("src = ", src)
+	//fmt.Println("ranges = ", ranges)
+
+	for _, i := range ranges {
+		lo := i.Source
+		hi := i.Source + i.Length
+		// fmt.Println("lo = ", lo, ", hi = ", hi)
+		if src >= lo && src < hi {
+			// fmt.Println("src ", src, " is in between ", lo, " and ", hi-1)
+			dest = i.Destination + src - lo
+			break
+		}
+	}
+	//fmt.Fprintln(os.Stderr, "src = ", src, ", dest = ", dest)
+	return dest
+}
+
+func mapSeedToLocation(a int, s, f, w, l, t, h, z MappingList) int {
+	soil := mapSrcToDest(a, s)
+	fertilizer := mapSrcToDest(soil, f)
+	water := mapSrcToDest(fertilizer, w)
+	light := mapSrcToDest(water, l)
+	temperature := mapSrcToDest(light, t)
+	humidity := mapSrcToDest(temperature, h)
+	location := mapSrcToDest(humidity, z)
+	return location
+}
+
 func main() {
 	var parserState State = Seeds
-	fmt.Fprintln(os.Stderr, parserState)
 
 	var toBePlanted SeedList
 	var soilMaps []Category
@@ -56,9 +87,15 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		line = strings.TrimSpace(line)
+		if len(line) == 0 {
+			continue // skip blank lines
+		}
+		// fmt.Fprintf(os.Stderr, "line: %s\n", line)
 		left, right, error := splitByColon(line)
-		if error != nil {
-			fmt.Fprintln(os.Stderr, "Error: ", error)
+		// fmt.Fprintln(os.Stderr, "left:", left)
+		// fmt.Fprintln(os.Stderr, "right:", right)
+		// fmt.Fprintln(os.Stderr, "error:", error)
+		if error == nil {
 			if strings.EqualFold(left, "seeds") {
 				parserState = Seeds
 				str := right
@@ -71,6 +108,7 @@ func main() {
 						os.Exit(1)
 					}
 					toBePlanted = append(toBePlanted, num)
+					//fmt.Fprintln(os.Stderr, "added seed: ", num)
 				}
 			} else if strings.EqualFold(left, "seed-to-soil map") {
 				parserState = Soil
@@ -90,44 +128,60 @@ func main() {
 				fmt.Fprintln(os.Stderr, "Unknown map")
 				os.Exit(1)
 			}
-
-		}
-		if len(line) == 0 {
-			continue // skip blank lines
+			// fmt.Fprintln(os.Stderr, "parserState = ", parserState)
+			continue
 		}
 		var categoryMap Category
 
-		_, err := fmt.Sscanf(line, "%d %d %d", &categoryMap.Destination, &categoryMap.Source, &categoryMap.Length)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error scanning integers: ", err)
-			os.Exit(1)
-		}
-		switch parserState {
-		case Seeds:
-			fmt.Println("State is Seeds")
-		case Soil:
-			soilMaps = append(soilMaps, categoryMap)
-			fmt.Fprintln(os.Stderr, "added Soil map: ", categoryMap)
-		case Fertilizer:
-			fertilizerMaps = append(fertilizerMaps, categoryMap)
-			fmt.Fprintln(os.Stderr, "added Fertilizer map: ", categoryMap)
-		case Water:
-			waterMaps = append(waterMaps, categoryMap)
-			fmt.Fprintln(os.Stderr, "added Water map: ", categoryMap)
-		case Light:
-			lightMaps = append(lightMaps, categoryMap)
-			fmt.Fprintln(os.Stderr, "added Light map: ", categoryMap)
-		case Temperature:
-			temperatureMaps = append(temperatureMaps, categoryMap)
-			fmt.Fprintln(os.Stderr, "added Temperature map: ", categoryMap)
-		case Humidity:
-			humidityMaps = append(humidityMaps, categoryMap)
-			fmt.Fprintln(os.Stderr, "added Humidity map: ", categoryMap)
-		case Location:
-			locationMaps = append(locationMaps, categoryMap)
-			fmt.Fprintln(os.Stderr, "added Location map: ", categoryMap)
-		default:
-			fmt.Println("Unknown State")
+		if parserState != Seeds {
+			_, err := fmt.Sscanf(line, "%d %d %d", &categoryMap.Destination, &categoryMap.Source, &categoryMap.Length)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error scanning integers: ", err)
+				os.Exit(1)
+			}
+			switch parserState {
+			case Soil:
+				soilMaps = append(soilMaps, categoryMap)
+				//fmt.Fprintln(os.Stderr, "added Soil map: ", categoryMap)
+			case Fertilizer:
+				fertilizerMaps = append(fertilizerMaps, categoryMap)
+				//fmt.Fprintln(os.Stderr, "added Fertilizer map: ", categoryMap)
+			case Water:
+				waterMaps = append(waterMaps, categoryMap)
+				//fmt.Fprintln(os.Stderr, "added Water map: ", categoryMap)
+			case Light:
+				lightMaps = append(lightMaps, categoryMap)
+				//fmt.Fprintln(os.Stderr, "added Light map: ", categoryMap)
+			case Temperature:
+				temperatureMaps = append(temperatureMaps, categoryMap)
+				//fmt.Fprintln(os.Stderr, "added Temperature map: ", categoryMap)
+			case Humidity:
+				humidityMaps = append(humidityMaps, categoryMap)
+				//fmt.Fprintln(os.Stderr, "added Humidity map: ", categoryMap)
+			case Location:
+				locationMaps = append(locationMaps, categoryMap)
+				//fmt.Fprintln(os.Stderr, "added Location map: ", categoryMap)
+			default:
+				fmt.Fprintln(os.Stderr, "Unknown State")
+				os.Exit(1)
+			}
 		}
 	}
+
+	//fmt.Println(mapSeedToLocation(79, soilMaps, fertilizerMaps, waterMaps, lightMaps, temperatureMaps, humidityMaps, locationMaps))
+	//fmt.Println(mapSeedToLocation(14, soilMaps, fertilizerMaps, waterMaps, lightMaps, temperatureMaps, humidityMaps, locationMaps))
+	//fmt.Println(mapSeedToLocation(55, soilMaps, fertilizerMaps, waterMaps, lightMaps, temperatureMaps, humidityMaps, locationMaps))
+	//fmt.Println(mapSeedToLocation(13, soilMaps, fertilizerMaps, waterMaps, lightMaps, temperatureMaps, humidityMaps, locationMaps))
+
+	var lowest int = math.MaxInt
+
+	for _, num := range toBePlanted {
+
+		//fmt.Println("Index:", i, "Number:", num)
+		x := mapSeedToLocation(num, soilMaps, fertilizerMaps, waterMaps, lightMaps, temperatureMaps, humidityMaps, locationMaps)
+		if x < lowest {
+			lowest = x
+		}
+	}
+	fmt.Println(lowest)
 }
