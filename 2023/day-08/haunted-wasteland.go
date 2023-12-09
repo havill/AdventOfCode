6c97ceb5b48f2c4520d5d466f6a0a8363f4a6844
Eido Inoue <havill@gmail.com>
2023-12-09 11:59:01 +0900

    second gold star achieved with brute force, fast computer, and time

--- 2023/day-08/haunted-wasteland.go
+++ 2023/day-08/haunted-wasteland.go
@@ -51,13 +51,35 @@ func isEndingNode(label string) bool {
 	return strings.HasSuffix(label, "Z")
 }
 
+func ghostsAreHome(ghostIsHere []*node) bool {
+	for _, ghost := range ghostIsHere {
+		if !isEndingNode(ghost.label) {
+			return false
+		}
+	}
+	return true
+}
+
+func debugGhosts(ghostIsHere []*node, count int) bool {
+	arrived := 0
+
+	for i, ghost := range ghostIsHere {
+		fmt.Fprint(os.Stdout, "Ghost #", i, ":", ghost.label)
+		if isEndingNode(ghost.label) {
+			fmt.Fprint(os.Stdout, " (home)")
+			arrived += 1
+		}
+		fmt.Fprintln(os.Stdout, "")
+	}
+	fmt.Fprintln(os.Stdout, "---------------------")
+	return arrived > count
+}
+
 func main() {
 	var instructions string
 	var instructionsI int = 0
 	var steps int = 0
 	var network Network = make(Network)
-	var youAreHere *node
-	var ghosts []*node
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -71,7 +93,6 @@ func main() {
 			instructions = caps
 		} else {
 			n := parseInput(line)
-			//fmt.Println("node: ", n)
 			addNodeToNetwork(network, n)
 		}
 	}
@@ -79,31 +100,70 @@ func main() {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 		os.Exit(1)
 	}
+	var youAreHere *node
 
 	youAreHere = network["AAA"]
 	for youAreHere != nil && youAreHere.label != "ZZZ" {
 		if instructionsI >= len(instructions) {
 			instructionsI = 0
 		}
-		//fmt.Fprintln(os.Stdout, "You are at: ", youAreHere.label, " instructions: ", instructionsI)
 		if instructions[instructionsI] == 'L' {
 			left := network[youAreHere.left].label
 			youAreHere = network[left]
-			//fmt.Fprintln(os.Stdout, "You went left to: ", youAreHere.label)
 		} else if instructions[instructionsI] == 'R' {
 			right := network[youAreHere.right].label
 			youAreHere = network[right]
-			//fmt.Fprintln(os.Stdout, "You went right to: ", youAreHere.label)
 		} else {
-			//fmt.Fprintln(os.Stderr, "Invalid instruction: ", instructions[instructionsI])
+			fmt.Fprintln(os.Stderr, "Invalid instruction: ", instructions[instructionsI])
 			os.Exit(1)
 		}
 		steps += 1
 		instructionsI += 1
 	}
-	if youAreHere == nil {
-		fmt.Fprintln(os.Stderr, "You are lost!")
+	fmt.Println(steps)
+	steps = 0
+
+	var ghostIsHere []*node
+
+	for key, value := range network {
+		if isStartingNode(key) {
+			ghostIsHere = append(ghostIsHere, value)
+		}
+	}
+	instructionsI = 0
+	for ghostIsHere != nil && !ghostsAreHome(ghostIsHere) {
+		/*
+			if debugGhosts(ghostIsHere, math.MaxInt32) {
+				os.Exit(1)
+			}
+		*/
+		if instructions[instructionsI] == 'L' {
+			//fmt.Fprint(os.Stdout, "GO LEFT (#", instructionsI, ") ")
+			for i, ghost := range ghostIsHere {
+				left := network[ghost.left].label
+				ghostIsHere[i] = network[left]
+			}
+		} else if instructions[instructionsI] == 'R' {
+			//fmt.Fprint(os.Stdout, "GO RIGHT (#", instructionsI, ") ")
+			for i, ghost := range ghostIsHere {
+				right := network[ghost.right].label
+				ghostIsHere[i] = network[right]
+			}
+		} else {
+			fmt.Fprintln(os.Stderr, "Invalid instruction: ", instructions[instructionsI])
+			os.Exit(1)
+		}
+		steps += 1
+		//fmt.Fprint(os.Stdout, "[STEP ", steps, "]\n\n")
+		instructionsI += 1
+		if instructionsI >= len(instructions) {
+			instructionsI = 0
+		}
+	}
+	if ghostIsHere == nil {
+		fmt.Fprintln(os.Stderr, "Ghosts are lost!")
 		os.Exit(1)
 	}
 	fmt.Println(steps)
+
 }
