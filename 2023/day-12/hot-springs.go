300aaaf5eb969f06dd4f8ab171953f16881c96bb
Eido Inoue <eido.a.inoue@rakuten.com>
2023-12-13 13:07:00 +0900

    naiive part 2 done

--- 2023/day-12/hot-springs.go
+++ 2023/day-12/hot-springs.go
@@ -99,12 +99,30 @@ func sumArray(numbers []int) int {
 	return sum
 }
 
+func repeatString(conditions string, folds int) string {
+	repeatedConditions := strings.Repeat(conditions+"?", folds)
+	return strings.TrimSuffix(repeatedConditions, "?") // Remove the trailing "?"
+}
+
+func repeatIntSlice(contiguousGroups []int, folds int) []int {
+	result := make([]int, 0, len(contiguousGroups)*folds)
+	for i := 0; i < folds; i++ {
+		result = append(result, contiguousGroups...)
+	}
+	return result
+}
+
 func main() {
 	total := 0
+	unfoldedTotal := 0
 	scanner := bufio.NewScanner(os.Stdin)
 
 	//fmt.Println("Please enter some lines of text. Press CTRL+D to end input.")
 	for scanner.Scan() {
+		var sum int
+		var pattern string
+		var re *regexp.Regexp
+
 		line := scanner.Text()
 		//fmt.Println("You entered: ", line)
 		left, right := splitString(line)
@@ -116,21 +134,35 @@ func main() {
 			return
 		}
 		//fmt.Println("Broken groups: ", brokenGroups)
-		sum := sumArray(brokenGroups)
+		sum = sumArray(brokenGroups)
 		//fmt.Println("Sum: ", sum)
-		pattern := convertSliceToString(brokenGroups)
+		pattern = convertSliceToString(brokenGroups)
 		//fmt.Println("Uncompiled: ", pattern)
-		re, err := regexp.Compile(pattern)
+		re, err = regexp.Compile(pattern)
 		if err != nil {
 			fmt.Println("Error compiling regex:", err)
 			return
 		}
-		matches := testAllCombos(re, sum, left)
+		total += testAllCombos(re, sum, left)
+
+		// Part 2
+
+		unfoldedConditions := repeatString(left, 5)
+		unfoldedBrokenGroups := repeatIntSlice(brokenGroups, 5)
+		//fmt.Println("Unfolded conditions: ", unfoldedConditions)
+		//fmt.Println("Unfolded broken groups: ", unfoldedBrokenGroups)
+		sum = sumArray(unfoldedBrokenGroups)
+		//fmt.Println("Sum: ", sum)
+		pattern = convertSliceToString(unfoldedBrokenGroups)
+		//fmt.Println("Uncompiled: ", pattern)
+		re, _ = regexp.Compile(pattern)
+		matches := testAllCombos(re, sum, unfoldedConditions)
 		//fmt.Println("Matches: ", matches)
-		total += matches
+		unfoldedTotal += matches
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 	fmt.Println("Total matches (part 1): ", total)
+	fmt.Println("Total matches (part 2): ", unfoldedTotal)
 }
