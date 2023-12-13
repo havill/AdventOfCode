084046c9644b9aea85aec042492a3cdc15521d97
Eido Inoue <eido.a.inoue@rakuten.com>
2023-12-13 11:53:24 +0900

    unoptimized but working for examples

--- 2023/day-12/hot-springs.go
+++ 2023/day-12/hot-springs.go
@@ -4,9 +4,39 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
+
+func groupMatchesRecord(re *regexp.Regexp, conditionRecord string) bool {
+	match := re.MatchString(conditionRecord)
+	//fmt.Println("groupMatchesRecord: ", conditionRecord, match)
+	return match
+}
+
+func testCombosHelper(re *regexp.Regexp, brokenGroupsSum int, conditionRecord string, current string) int {
+	total := 0
+
+	if len(conditionRecord) == 0 {
+		if groupMatchesRecord(re, current) {
+			fmt.Println(current)
+			total++
+		}
+		return total
+	}
+	if conditionRecord[0] == '?' {
+		total += testCombosHelper(re, brokenGroupsSum, conditionRecord[1:], current+".")
+		total += testCombosHelper(re, brokenGroupsSum, conditionRecord[1:], current+"#")
+	} else {
+		total += testCombosHelper(re, brokenGroupsSum, conditionRecord[1:], current+string(conditionRecord[0]))
+	}
+	return total
+}
+
+func testAllCombos(re *regexp.Regexp, brokenGroupsSum int, conditionRecord string) int {
+	return testCombosHelper(re, brokenGroupsSum, conditionRecord, "")
+}
 
 func splitString(input string) (string, string) {
 	parts := strings.Split(input, " ")
@@ -18,7 +48,6 @@ func splitString(input string) (string, string) {
 		if part == "" {
 			continue
 		}
-
 		if strings.ContainsAny(part, ".#?") {
 			left += part
 		} else {
@@ -45,26 +74,60 @@ func stringToIntSlice(input string) ([]int, error) {
 	return result, nil
 }
 
+func convertSliceToString(defectives []int) string {
+	var result []string
+
+	result = append(result, "^[^#]*")
+	for i, defective := range defectives {
+		result = append(result, fmt.Sprintf("[#?]{%d}", defective))
+		if i < len(defectives)-1 {
+			result = append(result, "[^#]+")
+		}
+	}
+	result = append(result, "[^#]*$")
+	return strings.Join(result, "")
+}
+
+func sumArray(numbers []int) int {
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
+}
+
 func main() {
+	total := 0
 	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Println("Please enter some lines of text. Press CTRL+D to end input.")
-
+	//fmt.Println("Please enter some lines of text. Press CTRL+D to end input.")
 	for scanner.Scan() {
 		line := scanner.Text()
 		fmt.Println("You entered: ", line)
 		left, right := splitString(line)
-		fmt.Println("Left: ", left)
-		fmt.Println("Right: ", right)
+		//fmt.Println("Left: ", left)
+		//fmt.Println("Right: ", right)
 		brokenGroups, err := stringToIntSlice(right)
 		if err != nil {
 			fmt.Println("Error: ", err)
-			continue
+			return
 		}
-		fmt.Println("Broken groups: ", brokenGroups)
+		//fmt.Println("Broken groups: ", brokenGroups)
+		sum := sumArray(brokenGroups)
+		fmt.Println("Sum: ", sum)
+		pattern := convertSliceToString(brokenGroups)
+		//fmt.Println("Uncompiled: ", pattern)
+		re, err := regexp.Compile(pattern)
+		if err != nil {
+			fmt.Println("Error compiling regex:", err)
+			return
+		}
+		matches := testAllCombos(re, sum, left)
+		fmt.Println("Matches: ", matches)
+		total += matches
 	}
-
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
+	fmt.Println("Total matches (part 1): ", total)
 }
